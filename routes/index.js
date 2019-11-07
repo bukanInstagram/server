aaca131624180046435d3c56607b061bc1dd6ad5
dadepe <rizky_dwifutra@yahoo.com>
2019-11-07 19:32:04 +0700

    upload

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 const routes = require('express').Router();
+const images = require('../helpers/images')
 
 routes.use('/posts', require('./posts'));
 routes.use('/users', require('./users'));
 routes.use('/comments', require('./comments'));
+routes.post('/upload',
+  images.multer.single('image'), 
+  images.sendUploadToGCS,
+  (req, res) => {
+    res.send({
+      status: 200,
+      message: 'Your file is successfully uploaded',
+      link: req.file.cloudStoragePublicUrl
+    })
+  })
 
 module.exports = routes;
