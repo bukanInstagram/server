e9732a902a19824f4ad70eb92948cf3e98930cf4
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-11-07 15:54:11 +0700

    init commit

---
+++ app.js
@@ -0,0 +1,21 @@
+if (process.env.NODE_ENV === 'development') require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const PORT = process.env.PORT;
+const mongoose = require('mongoose');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use('/', require('./routes'));
+
+mongoose
+	.connect(process.env.MONGODV_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('mongodb connection success'))
+	.catch(() => console.log('mongodb connection failed'));
+
+app.listen(PORT, () => console.log('app listening to port', PORT));
